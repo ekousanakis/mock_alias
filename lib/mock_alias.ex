967493c7bd47061cc127b9b5aa7f8bde99b25bab
Manolis Kousanakis <m.kousanakis@stoiximan.gr>
2019-06-12 00:32:56 +0300

    refactor code

--- lib/mock_alias.ex
+++ lib/mock_alias.ex
@@ -7,62 +7,65 @@ defmodule MockPathAlias do
     end
   end
 
-  defmacro mock_alias(mock_alias, opts \\ [])
+  defmacro mock_alias(ast_mock_alias, opts \\ [])
+
+  defmacro mock_alias(ast_mock_alias, opts) do
+    Mix.env()
+    |> do_mock_alias(ast_mock_alias, __CALLER__, opts)
+    |> expand_macros(opts)
+  end
 
   # alias Foo.Bar.Buzz, as: BuzzBarFoo
   # alias Foo.Bar.Buzz
-  defmacro mock_alias({:__aliases__, _, _} = ast_mock_alias, opts) do
-    new_mocked_alias =
-      if Mix.env() === :test do
-        mock_alias = Macro.expand(ast_mock_alias, __CALLER__)
+  def do_mock_alias(:test, {:__aliases__, _, _} = ast_mock_alias, env, _opts) do
+    mock_alias = Macro.expand(ast_mock_alias, env)
 
-        @app
-        |> Application.get_env(:mock_alias)
-        |> Enum.find({nil, nil}, &(elem(&1, 0) == mock_alias))
-        |> elem(1)
-        |> validate(mock_alias)
-      else
-        ast_mock_alias
-      end
+    {_, new_mocked_alias} =
+      @app
+      |> Application.get_env(:mock_alias)
+      |> Enum.find({nil, nil}, &(elem(&1, 0) == mock_alias))
 
-    expand_macro(new_mocked_alias, opts)
+    validate(new_mocked_alias, mock_alias)
+
+    [new_mocked_alias]
   end
 
   # alias Foo.Bar.{Baz, Buzz}
-  defmacro mock_alias({{:., _, [{_, _, root_path}, :{}]}, _, aliases} = ast_mock_aliases, _opts) do
-    new_mocked_aliases =
-      if Mix.env() == :test do
-        mock_alias_list = @app |> Application.get_env(:mock_alias)
+  def do_mock_alias(:test, ast_mock_aliases, _env, _opts) do
+    {{:., _, [{_, _, root_path}, :{}]}, _, aliases} = ast_mock_aliases
 
-        Enum.map(aliases, fn {_, _, each_alias} ->
-          mock_alias = Module.concat(root_path ++ each_alias)
+    pairs_with_mock_aliases = @app |> Application.get_env(:mock_alias)
 
-          mock_alias_list
-          |> Enum.find({nil, nil}, &(elem(&1, 0) === mock_alias))
-          |> elem(1)
-          |> validate(mock_alias)
-        end)
-      else
-        [ast_mock_aliases]
-      end
+    Enum.map(aliases, fn {_, _, each_alias} ->
+      mock_alias = Module.concat(root_path ++ each_alias)
 
-    Enum.map(new_mocked_aliases, &expand_macro(&1))
+      {_, new_mocked_alias} =
+        pairs_with_mock_aliases
+        |> Enum.find({nil, nil}, &(elem(&1, 0) === mock_alias))
+
+      validate(new_mocked_alias, mock_alias)
+
+      new_mocked_alias
+    end)
   end
 
+  def do_mock_alias(_, mock_alias), do: [mock_alias]
+
   defp validate(new_mocked_alias, target_mock_alias) do
-    cond do
-      new_mocked_alias === nil ->
-        raise "declare mocked alias for the #{inspect(target_mock_alias)}"
+    unless new_mocked_alias !== nil do
+      raise "declare mocked alias for the #{inspect(target_mock_alias)}"
+    end
 
-      target_mock_alias.__info__(:functions) !== new_mocked_alias.__info__(:functions) ->
-        raise "declare mocking function(s) for the #{inspect(new_mocked_alias)} module"
-
-      true ->
-        new_mocked_alias
+    unless target_mock_alias.__info__(:functions) === new_mocked_alias.__info__(:functions) do
+      raise "declare mocking function(s) for the #{inspect(new_mocked_alias)} module"
     end
   end
 
-  defp expand_macro(mocked_alias) do
+  defp expand_macros(mocked_alias, opts) do
+    Enum.map(mocked_alias, &expand_macro(&1, opts))
+  end
+
+  defp expand_macro(mocked_alias, []) do
     quote do: alias(unquote(mocked_alias))
   end
 
